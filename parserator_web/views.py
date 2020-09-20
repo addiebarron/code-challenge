ce983ca51b7f33863ee95b7964df24f364a8fb38
addiebarron <addiebarronemail@gmail.com>
2020-09-20 02:23:15 -0400

    API view done, form wiring done, parsing working, error handling working, no testing yet.

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,9 +16,30 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+
+        address = request.query_params.get('address');
+
+        try:
+            address_components, address_type = self.parse(address);
+            response = {
+                'components': address_components,
+                'type': address_type
+            }
+        except Exception as exception:
+            exception_name = type(exception).__name__
+            response = {
+                'error': True,
+                'exceptionName': exception_name,
+                'detail': 'Unable to parse.'
+                # Finer exception handling would be a nice next step I think
+            }
+
+        return Response(response)
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+
+        address_components, address_type = usaddress.tag(address);
+
         return address_components, address_type
